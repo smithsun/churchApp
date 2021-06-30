68fd501074d51b61fc19f297b8dca10547fff74e
smith.sun@pollex.com.tw <smith.sun@pollex.com.tw>
2021-06-30 20:35:56 +0800

    在login 元件，加入 translation service, 一進入即顯示正確的翻譯

--- src/main/webapp/app/account/login-form/login-form.component.ts
+++ src/main/webapp/app/account/login-form/login-form.component.ts
@@ -2,6 +2,7 @@ import axios from 'axios';
 import Component from 'vue-class-component';
 import { Vue, Inject } from 'vue-property-decorator';
 import AccountService from '@/account/account.service';
+import TranslationService from '@/locale/translation.service';
 @Component({
   watch: {
     $route() {
@@ -10,12 +11,17 @@ import AccountService from '@/account/account.service';
   },
 })
 export default class LoginForm extends Vue {
-  @Inject('accountService')
-  private accountService: () => AccountService;
+  @Inject('accountService') private accountService: () => AccountService;
+  @Inject('translationService') private translationService: () => TranslationService;
   public authenticationError = null;
   public login = null;
   public password = null;
   public rememberMe: boolean = null;
+  private currentLanguage = this.$store.getters.currentLanguage;
+  // private currentLanguage = 'zh-tw';
+  created() {
+    this.translationService().refreshTranslation(this.currentLanguage);
+  }
 
   public doLogin(): void {
     const data = { username: this.login, password: this.password, rememberMe: this.rememberMe };
